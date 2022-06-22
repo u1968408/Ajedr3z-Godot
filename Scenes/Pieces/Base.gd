11022ebe9554dc7e9635ce7208e1cae6d1b33f6e
RubyEnoshima <vilaruben70@gmail.com>
2022-06-22 03:47:58 +0200

    Pieza base donete uwu

--- Scenes/Pieces/Base.gd
+++ Scenes/Pieces/Base.gd
@@ -2,10 +2,26 @@ extends Node2D
 
 export var casillasDisponibles = [] # Vector2(1,1)...
 export var blanca = true
+export var pos = Vector2(0,0)
 
 func _ready():
 	pass # Replace with function body.
 
-func comprobarMovimiento(tablero):
+# Devuelve un array de los posibles movimientos que puede hacer la pieza
+func posiblesMovimientos(tablero):
+	var res = []
 	for casilla in casillasDisponibles:
-		
+		var casillaResultante = Vector2(pos.x+casilla.x,pos.y+casilla.y)
+		if casillaResultante.x >= 0 and casillaResultante.x < tablero.size() and casillaResultante.y >= 0 and casillaResultante.y < tablero[casillaResultante.x].size() and tablero[casillaResultante.x][casillaResultante.y]==0:
+			res.append(casillaResultante)
+	return res
+	
+# Devuelve true si el movimiento se puede hacer
+func comprobarMovimiento(tablero, movimiento):
+	return movimiento in posiblesMovimientos(tablero)
+
+# Si el movimiento es plausible, lo realiza
+func hacerMovimiento(tablero, movimiento):
+	if comprobarMovimiento(tablero,movimiento):
+		tablero[pos.x][pos.y] = 0
+		tablero[movimiento.x][movimiento.y] = blanca
