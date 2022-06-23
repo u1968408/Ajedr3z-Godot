977c800704cc231b169207ac1455cf321cec9434
RubyEnoshima <vilaruben70@gmail.com>
2022-06-23 18:19:57 +0200

    jeje huevos hervidos

--- Scenes/Players/Weapons/Fletxa.gd
+++ Scenes/Players/Weapons/Fletxa.gd
@@ -10,7 +10,7 @@ func _ready():
 	pass 
 
 func _physics_process(delta):
-	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
+	move_and_collide(velocity.normalized() * delta * speed)
 
 
 func _on_Timer_timeout(): # Eliminem la fletxa passat un temps
